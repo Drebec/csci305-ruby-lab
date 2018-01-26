454735f8c56fc5d69ede48c396b8a218140fbb9a
Drew Beck <drew.beck33@gmail.com>
2018-01-26 10:35:04 -0700

    Questions 1 - 13 auto-answered. Stop words filtered. No fix for looping.

--- ruby_lab.rb
+++ ruby_lab.rb
@@ -12,16 +12,24 @@
 $bigrams = Hash.new # The Bigram data structure
 $name = "Drew Beck" # Me
 $counter = 0	# Tracks each time a new valid song title is processes in the bigram
+$stop_words = ["a", "an", "and", "by", "for", "from", "in", "of", "on", "or", "out", "the", "to", "with"]
 
 # Function to process each line of a file and extract the song titles
 def process_file(file_name)
 	puts "Processing File.... "
 
 	begin
-		IO.foreach(file_name) do |line|
+		#IO.foreach(file_name) do |line|
+		size = File.new(file_name).size	# This is the only way I've been able to get the entire unique_tracks.txt to be read
+		file = File.open(file_name)
+		lines = file.read(size)
+		lines_array = lines.split("\n")
+		lines_array.each do |line|	# For each line of the file
+			line += "\n"	# Above file parsing splits lines on the \n character, but the cleanup_title
+			              # method uses the \n charaacter in the non-English characters check so it needs to be added back
 			title = cleanup_title(line)	# Removes unwanted text and sets string to downcase
 			build_bigram(title)	# Builds the bigram structure
-		end # do IO.foreach
+		end # do lines.each
 		puts "Finished. Bigram model built.\n"
 	rescue # begin
 		STDERR.puts "Could not open file"
@@ -51,24 +59,27 @@ def build_bigram title	# Populates the $bigrams hash
 		title.downcase!	# Reduce case of title
 		$counter += 1	# A new valid song title is being processed
 		words = title.split(' ')	# Separate words
-		index = 0	# Tracks the index of the current word. Used to ignore the last word of every song
-		words.each do |word|	# For each word
-			if $bigrams[word] == nil # If it doesn't already exist in the bigram, create a new entry for it and the following
-															 # word and initialize the value to 1
-				$bigrams[word] = Hash.new
-				$bigrams[word][words[index+1]] = 1
-			elsif $bigrams[word][words[index+1]] == nil	# If the current word exists but the following word does not
-																									# create the entry and initialize the value to 1
-				$bigrams[word][words[index+1]] = 1
-			else
-				$bigrams[word][words[index+1]] += 1	# If the entry already exists, increment the value
-			end # if $bigrams[word] == nil
-			index += 1	# Increment the word index
-			if index > words.length - 2	# If the index is at the final word, stop the loop, thus ignoring the last word of each title
-				break
-			end # if index > words.length -2
-		end # do words.each
-	end # if title =~ regex
+		words -= $stop_words
+		if words.length != 1
+			index = 0	# Tracks the index of the current word. Used to ignore the last word of every song
+			words.each do |word|	# For each word
+				if $bigrams[word] == nil # If it doesn't already exist in the bigram, create a new entry for it and the following
+																 # word and initialize the value to 1
+					$bigrams[word] = Hash.new
+					$bigrams[word][words[index+1]] = 1
+				elsif $bigrams[word][words[index+1]] == nil	# If the current word exists but the following word does not
+																										# create the entry and initialize the value to 1
+					$bigrams[word][words[index+1]] = 1
+				else
+					$bigrams[word][words[index+1]] += 1	# If the entry already exists, increment the value
+				end # if $bigrams[word] == nil
+				index += 1	# Increment the word index
+				if index > words.length - 2	# If the index is at the final word, stop the loop, thus ignoring the last word of each title
+					break
+				end # if index > words.length -2
+			end # do words.each
+		end # if words.length != 1
+	end # if title != nil
 end # build_bigram str
 
 def mcw str	# Returns the most common word to follow the input str
@@ -89,16 +100,35 @@ def mcw str	# Returns the most common word to follow the input str
 end # def mcw str
 
 def get_random_word str
-	$bigrams[str].keys.each do |key|
-
-	end # do $bigrams[str].keys.each
+	if $bigrams[str] != nil
+		len = $bigrams[str].keys.length
+		keys = $bigrams[str].keys
+		index = rand(len)
+		return keys[index]
+	end
+	return ""
 end # get_next_word str
+
+def create_random_title str	# Creates a 20 string title using the mcw function
+	current = str	# The current string
+	created_title = "#{current}"	# String being created
+	if (get_random_word current) != nil	# As long as the mcw exists for the current word
+		(0..18).each do 	# Do until it stops
+			created_title += " #{get_random_word current}"	# Add the mcw to the end of the created_title string
+			current = (get_random_word current)	# Get the next mcw
+			if current == nil
+				break
+			end # end if current == nil
+		end # while TRUE
+	end # if mcw str != nil
+	return created_title	# Return result
+end # create_title str
 
 def create_title str	# Creates a 20 string title using the mcw function
 	current = str	# The current string
 	created_title = "#{current}"	# String being created
 	if (mcw current) != nil	# As long as the mcw exists for the current word
-		(0..19).each do 	# Do 20 times
+		(0..18).each do 	# Do 20 times
 			created_title += " #{mcw current}"	# Add the mcw to the end of the created_title string
 			current = (mcw current)	# Get the next mcw
 		end # do 0..20
@@ -123,6 +153,7 @@ def main_loop()
 	#puts "The most common word to follow 'computer' is '#{mcw("computer")}'"
 	#puts "The word 'computer' has #{$bigrams['computer'].count} unique words that follow it"
 	#puts "The most common word to follow 'love' is '#{mcw("love")}'"
+	#puts $bigrams['happy']
 	#puts create_title("happy")
 	#puts $counter
 
